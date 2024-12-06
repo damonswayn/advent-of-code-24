7caaaa8526b214739ad53fe2631f4adc414a2c35
Damon Swayn <damon.swayn@catch.com.au>
2024-12-06 19:56:29 +1100

    make the make_src_dir.sh smart enough to inspect existing folders and generate the next day without requiring arguments, also make it an npm script.

--- make_src_dir.sh
+++ make_src_dir.sh
@@ -4,22 +4,36 @@
 # Usage: ./make_src_dir.sh <day_number> where <day_number> is the day of the
 # advent of code challenge as 2 digits (01-25)
 
+# Function to get the highest day number from the src directory
+get_highest_day_number() {
+    find src -maxdepth 1 -type d -regex 'src/[0-9][0-9][ab]' | \
+    sed -E 's/src\/([0-9][0-9])[ab]/\1/' | \
+    sort -n | \
+    tail -n 1
+}
+
 # Check if the number of arguments is correct
 if [ "$#" -ne 1 ]; then
-    echo "Usage: ./make_src_dir.sh <day_number>"
-    exit 1
+    highest_day=$(get_highest_day_number)
+    if [ -z "$highest_day" ]; then
+      echo "No day number provided and no existing directories found. Please provide a day number."
+      exit 1
+    fi
+    day_number=$(printf "%02d" "$((10#$highest_day + 1))")
+else
+    day_number=$1
 fi
 
 # Check if the day number is valid
-if ! echo "$1" | grep -Eq '^([012][0-9])$'; then
+if ! echo "$day_number" | grep -Eq '^([012][0-9])$'; then
     echo "Invalid day number. Please enter a number between 01 and 25"
     exit 1
 fi
 
-echo "Starting advent of code template generation for day $1..."
+echo "Starting advent of code template generation for day $day_number..."
 
-PART_ONE_NAME="$1a"
-PART_TWO_NAME="$1b"
+PART_ONE_NAME="${day_number}a"
+PART_TWO_NAME="${day_number}b"
 
 # Create the source directory
 mkdir -p "src/$PART_ONE_NAME" "src/$PART_TWO_NAME"
